0aac2ad8202c940e524e68d5979dc039613e88bf
Kuliq <vikakulik687@gmail.com>
2018-09-25 19:04:48 +0300

    Добавила решения заданий Урока №2

---
+++ Lesson_2/Task_2/Task_2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Task_2
+    {
+        //Кулик В.В.
+        //2. Написать метод подсчета количества цифр числа
+
+        private static int Count(int a)
+        {
+            int col=1;
+            
+            for (int i = 1; a >= 10; i++)
+            {
+                a = a / 10;
+                col = ++col;
+            }
+            
+            return col;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            var num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Количество цифр в числе: {Count(num)}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Lesson_2/Task_6/Task_6.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    //Кулик В.В.
+    //6. * Написать программу подсчета количества «Хороших» чисел в диапазоне от 1 до 1 000 000 000. 
+    //Хорошим называется число, которое делится на сумму своих цифр.
+    //Реализовать подсчет времени выполнения программы, используя структуру DateTime.
+    class Task_6
+    {
+        // Метод подсчета суммы цифр числа
+        private static int Sum (int num)
+        {
+            int summa = 0;
+            for (var i = 0; num > 0; i++)
+            {
+                summa = summa + num % 10;
+                num = num / 10;
+            }
+            return summa;
+        }
+        static void Main(string[] args)
+        {
+            //задаем счетчик времени
+            DateTime One = DateTime.Now;
+
+            int num = 1;
+            int col = 0;
+            int ch = 10000000; //число диапазона подсчета хороших чисел
+            
+            while (num < ch) //задаем диапазон повторений
+            {
+                //Проверяем условие дилится ли число без остатка на сумму цифр
+                for (int i = 0; num % Sum(num) == 0; i++)
+                {
+                    col++;//если делится включаем счетчик
+                    num++;
+                }
+                num++;
+            }
+
+            DateTime Two = DateTime.Now; //задаем счетчик времени
+            TimeSpan Time = Two - One;
+            Console.WriteLine($"Время, затраченное на расчёт, мc: {Time.Milliseconds.ToString()}");
+            Console.WriteLine($"Количество \"хороших\" чисел от 1 до {ch}: {col}");
+            Console.ReadLine();
+        }
+    }
+}
